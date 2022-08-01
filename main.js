e8632b9dad82ac7e16b42b7c609b420d56d855bf
Pihu-Ananya <97041050+Pihu-Ananya@users.noreply.github.com>
2022-08-01 15:12:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song=""
+leftwristX=""
+leftwristY=""
+rigthwristX=""
+rightwristY=""
 
 function preload() {
 song=loadSound("")
@@ -9,8 +13,31 @@ function setup() {
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    poseNet=ml5.poseNet(video , modelloaded)
+    poseNet.on("pose", getposes)
+    }
 
-}
+    function getposes(results) {
+        if(results.length>0) {
+            console.log(results)
+            leftwristX=results[0].pose.leftWrist.x
+            leftwristY=results[0].pose.leftWrist.y 
+            rightwristX=results[0].pose.rightWrist.x
+            rightwristY=results[0].pose.rightWrist.y
+            console.log(leftwristX , leftwristY , rightwristX , rightwristY)
+            leftwrist_score=results[0].pose.keypoints[9].score
+            console.log(leftwrist_score)
+            rightwrist_score=results[0].pose.keypoints[10].score
+            console.log(rightwrist_score)
+    
+        }
+    }
+    
+    function modelloaded() {
+        console.log("model is loaded")
+    } 
+
+
 function draw() {
     image(video , 0 , 0 , 600 , 500);
 
